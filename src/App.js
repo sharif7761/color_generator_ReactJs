273175596a1af8a4f641ebcabd2f3ad9e35cb970
sharif7761 <sharif7761@gmail.com>
2021-04-25 01:22:57 +0600

    design setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
+  return (
+      <>
+        <section className='container'>
+          <h3>Color Generator</h3>
+          <form onSubmit={handleSubmit}>
+            <input type='text' value={color} placeholder='#f15025' onChange={(e) => setColor(e.target.value) }/>
+            <button className='btn' type='submit'>submit</button>
+          </form>
+        </section>
+        <section className='colors'>
+
+        </section>
+      </>
+  )
 }
 
 export default App
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-function componentToHex(c) {
+ function componentToHex(c) {
   var hex = c.toString(16);
   return hex.length == 1 ? '0' + hex : hex;
 }
