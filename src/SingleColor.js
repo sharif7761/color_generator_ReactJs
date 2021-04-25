e69454077fa6da3ab5000eddb2fefabf379decc2
sharif7761 <sharif7761@gmail.com>
2021-04-26 02:00:38 +0600

    fadeout clipboard text

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -7,6 +7,12 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const bcg = rgb.join(',')
   const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
+  useEffect(() =>{
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    },3000)
+    return ()=>clearTimeout(timeout)
+  },[alert])
   return (
       <article className={`color ${index > 10 && 'color-light' }`}
                style={{backgroundColor:`rgb(${bcg})`}}
