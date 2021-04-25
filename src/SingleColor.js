afbeb5a27ac6c4dd292b28151b567f603970fd15
sharif7761 <sharif7761@gmail.com>
2021-04-26 01:52:54 +0600

    copy color code to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -8,11 +8,20 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
   return (
-      <article className={`color ${index > 10 && 'color-light' }`} style={{backgroundColor:`rgb(${bcg})`}}>
+      <article className={`color ${index > 10 && 'color-light' }`}
+               style={{backgroundColor:`rgb(${bcg})`}}
+              onClick={() => {
+                setAlert(true)
+                navigator.clipboard.writeText(hexValue)
+              }}
+      >
         <p className='percent-value'>
           {weight}%
         </p>
         <p className='color-value'>{hexValue}</p>
+        {
+          alert && <p className='alert'>copied to clipboard</p>
+        }
       </article>
   )
 }
